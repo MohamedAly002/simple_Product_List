bce022b8535ea923bccadcb8257a711c4839bec4
MohamedAly002 <mo7amed3ly77@gmail.com>
2024-09-17 08:50:25 +0300

    -rename widgets folder to screens folder

---
+++ lib/screens/error_widget.dart
@@ -0,0 +1,35 @@
+import 'package:elevate_task/cubit/get%20product%20cubit/get_cubit_product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var productCubit = BlocProvider.of<GetPrductCubit>(context);
+
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'oops! something went wrong.\nplease check your connection',
+              style: TextStyle(color: Colors.black, fontSize: 24),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  productCubit.getProductData();
+                },
+                child: const Text('Try Again')),
+          ],
+        ),
+      ),
+    );
+  }
+}
